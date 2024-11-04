d404fc11e893659869e674d843b8f8fe8c6daa18
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-11-03 21:40:43 -0300

    Update ES.c

--- B-Tree/ES/ES.c
+++ B-Tree/ES/ES.c
@@ -1,7 +1,15 @@
+/*
+ES.c
+
+Descrição: implementação das funções responsávei por criar um dataset.
+*/
+
+/* --- Includes. --- */
+
 #include "ES.h"
 #include <stdlib.h>
 
-/*** -- Estruturas -- ***/
+/* --- Estruturas --- */
 
 struct linha {
     char nome[6];
@@ -9,32 +17,31 @@ struct linha {
     long long int cpf;
 };
 
-/*** -- Funções -- ***/
+/* --- Funções --- */
 
 void geraLinha(linha *linha, int seed) {
-    //Setando a seed.
+    /* Setando a seed. */
     srand(seed);
-    //Aleatorizando a data de nascimento.
+    /* Aleatorizando a data de nascimento. */
     linha->dia = (rand() % 31) + 1;
     linha->mes = (rand() % 12) + 1;
     linha->ano = (rand() % 75) + 1950;
-    //Aleatorizando o nome, usando a tabela ascII.
+    /* Aleatorizando o nome, usando a tabela ascII. */
     for(int i = 0; i < 5; i++) {
         linha->nome[i] = (char)((rand() % 26) + 97);
     }
-    //Aleatorizando o cpf.
+    /* Aleatorizando o cpf. */
     linha->cpf = (rand() % 1000000000000) + 10000000000;
 }
 
 int criaDataset(FILE *arq, int tam) {
-    linha linha;
-
-    //Colocando o byte nulo na última posição.
-    linha.nome[5] = '\0';
-    //Gerando o arquivo.
+    linha line;
+    /* Colocando o byte nulo na última posição. */
+    line.nome[5] = '\0';
+    /* Gerando o arquivo. */
     for(int i = 0; i < tam; i++) {
-        geraLinha(&linha, i);
-        fprintf(arq, "%05d %s %02d/%02d/%04d %lld\n", i, linha.nome, linha.dia, linha.mes, linha.ano, linha.cpf);
+        geraLinha(&line, i);
+        fprintf(arq, "%05d %s %02d/%02d/%04d %lld\n", i, line.nome, line.dia, line.mes, line.ano, line.cpf);
     }
     return 1;
 }
