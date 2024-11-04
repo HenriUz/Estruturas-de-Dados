fb5503d2e3798bdc88303639116cc9f952e7c1a3
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-11-04 11:32:26 -0300

    Update ArvoreRN.h

--- Arvore Rubro-Negra/Arvore/ArvoreRN.h
+++ Arvore Rubro-Negra/Arvore/ArvoreRN.h
@@ -1,7 +1,7 @@
 /*
 ArvoreRN.h
 
-Descrição: definição das funções da árvore rubro-negra.
+Descrição: definição das funções da árvore.
 */
 
 /* --- Constantes. --- */
@@ -18,86 +18,84 @@ typedef struct arvore arvore;
 /* --- Funções. --- */
 
 /*
-Descrição: função que cria e inicializa um novo nó.
+Descrição: função responsável por criar e inicializar um novo nó.
 Entrada: inteiro da chave.
 Saída: ponteiro para o nó.
 */
 no *criaNo(int chave);
 
 /*
-Descrição: função que cria e inicializa uma árvore.
+Descrição: função responsável por criar e inicializar uma árvore.
 Entrada: nada.
 Saída: ponteiro para a árvore.
 */
 arvore *criaArvore();
 
 /*
-Descrição: função que retorna a raiz da árvore.
+Descrição: função responsável por retornar a raiz da árvore.
 Entrada: ponteiro para a árvore.
 Saída: ponteiro para a raiz.
 */
 no *getRaiz(arvore *arv);
 
 /*
-Descrição: função que retorna a quantidade de elementos na árvore.
+Descrição: função responsável por retornar a quantidade de elementos na árvore.
 Entrada: ponteiro para a árvore.
 Saída: inteiro da quantidade de elementos.
 */
 int getNumElementos(arvore *arv);
 
 /*
-Descrição: função que insere um elemento na árvore.
+Descrição: função responsável por inserir um elemento na árvore.
 Entrada: ponteiro para a árvore, inteiro da chave.
 Saída: 1 - sucesso, 0 - erro.
 */
 int insere(arvore *arv, int chave);
 
 /*
-Descrição: função que remove um elemento da árvore.
+Descrição: função responsável por remover um elemento da árvore.
 Entrada: ponteiro para a árvore, inteiro da chave.
 Saída: 1 - sucesso, 0 - árvore vazia, −1 - elemento não encontrado.
 */
 int deleta(arvore *arv, int chave);
 
 /*
-Descrição: função que imprime a árvore em pré-ordem.
+Descrição: função responsável por imprimir a árvore em pré-ordem.
 Entrada: ponteiro para o nó.
 Saída: nada.
 */
 void preOrdem(no *elemento);
 
 /*
-Descrição: busca um elemento na árvore rubro-negra.
+Descrição: função responsável por buscar um elemento na árvore.
 Entrada: ponteiro para a árvore, inteiro da chave.
 Saída: cor do elemento - sucesso, −1 - elemento não encontrado.
 */
 int buscaElemento(arvore *arv, int chave);
 
-/* --- Funções de balanceamento da Rubro-Negra. --- */
-
 /*
-Descrição: função que verifica e corrige, se necessário, as propriedades da árvore após a inserção.
+Descrição: função responsável por verificar e corrigir, se necessário, as propriedades da árvore após a inserção.
 Entrada: ponteiro para a árvore, ponteiro para o nó inserido.
 Saída: nada.
 */
 void analisaInsercao(arvore *arv, no *folha);
 
 /*
-Descrição: função que verifica e corrige, se necessário, as propriedades da árvore após a remoção.
+Descrição: função responsável por verificar e corrigir, se necessário, as propriedades da árvore após a remoção.
 Entrada: ponteiro para a árvore, ponteiro para o nó removido.
 Saída: nada.
 */
 void analisaRemocao(arvore *arv, no *analisado);
 
 /*
-Descrição: função que realiza uma rotação para a esquerda.
+Descrição: função responsável por realizar uma rotação para a esquerda no nó informado.
 Entrada: ponteiro para o nó que será rotacionado.
 Saída: nada.
 */
 void rotacaoEsq(no *noDesbalanceado);
 
 /*
-Descrição: função que realiza uma rotação para a direita.
+Descrição: função responsável por realizar uma rotação para a direita no nó informado.
 Entrada: ponteiro para o nó que será rotacionado.
 Saída: nada.
 */
