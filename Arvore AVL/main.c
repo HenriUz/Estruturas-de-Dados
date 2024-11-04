83ad5eb9db9319dd6acc5c08692306f814c6be48
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-11-04 10:55:25 -0300

    Update main.c

--- Arvore AVL/main.c
+++ Arvore AVL/main.c
@@ -1,68 +1,77 @@
+/*
+main.c
+
+Descrição: programa responsável por lidar com os inputs do usuário e por gerenciar as funções da árvore.
+*/
+
+/* --- Includes. --- */
+
+#include "Arvore/ArvoreAvl.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "Arvore/ArvoreAvl.h"
+
+/* --- Funções. --- */
 
 int main(void) {
     arvore *arv;
     int resp, indice, retornoDeleta;
     char caractere;
-    /*** -- Criando a árvore. -- ***/
     arv = criaArvore();
     if(!arv) {
         return 1;
     }
-    /*** -- Menu. -- ***/
-    printf_s("\nMENU.\n1 - Inserir.\n2 - Remover.\n3 - Buscar.\n4 - Imprimir.\n5 - Sair\nSua resposta: ");
-    scanf_s("%d", &resp);
+    /* Iniciando interface. */
+    printf("\nMENU.\n1 - Inserir.\n2 - Remover.\n3 - Buscar.\n4 - Imprimir.\n5 - Sair\nSua resposta: ");
+    scanf("%d", &resp);
     while(resp != 5) {
         switch (resp) {
             case 1:
-                //Inserção.
-                printf_s("\nDigite o índice que será inserido: ");
-                scanf_s("%d", &indice);
-                printf_s("\nDigite o caractere que será inserido: ");
-                scanf_s(" %c", &caractere, (unsigned)sizeof(caractere));
+                /* Inserindo elemento. */
+                printf("\nDigite o índice que será inserido: ");
+                scanf("%d", &indice);
+                printf("\nDigite o caractere que será inserido: ");
+                scanf(" %c", &caractere);
                 if(!insere(arv, indice, caractere)) {
-                    printf_s("\nErro na inserção.\n");
+                    printf("\nErro na inserção.\n");
                 }else {
-                    printf_s("\nElemento inserido com sucesso.\n");
+                    printf("\nElemento inserido com sucesso.\n");
                 }
                 break;
             case 2:
-                //Remoção.
-                printf_s("\nDigite o índice que será removido: ");
-                scanf_s("%d", &indice);
+                /* Removendo elemento. */
+                printf("\nDigite o índice que será removido: ");
+                scanf("%d", &indice);
                 retornoDeleta = deleta(arv, indice);
                 if(retornoDeleta == 0) {
-                    printf_s("\nElemento não encontrado.\n");
+                    printf("\nElemento não encontrado.\n");
                 }else if(retornoDeleta == -1) {
-                    printf_s("\nÁrvore vazia.\n");
+                    printf("\nÁrvore vazia.\n");
                 }else {
-                    printf_s("\nElemento removido com sucesso.\n");
+                    printf("\nElemento removido com sucesso.\n");
                 }
                 break;
             case 3:
-                printf_s("\nDigite o índice que será buscado: ");
-                scanf_s("%d", &indice);
+                /* Buscando elemento. */
+                printf("\nDigite o índice que será buscado: ");
+                scanf("%d", &indice);
                 if(!busca(arv, indice)) {
-                    printf_s("\nElemento não encontrado.\n");
+                    printf("\nElemento não encontrado.\n");
                 }else {
-                    printf_s("\nElemento encontrado.\n");
+                    printf("\nElemento encontrado.\n");
                 }
                 break;
             case 4:
-                //Impreção.
-                printf_s("\nQuantidade de elementos na árvore: %d", getNumElementos(arv));
+                /* Imprimindo árvore. */.
+                printf("\nQuantidade de elementos na árvore: %d", getNumElementos(arv));
                 preOrdem(getRaiz(arv));
-                printf_s("\n");
+                printf("\n");
                 break;
             default:
-                //Nada.
-                break;
         }
-        printf_s("\nMENU.\n1 - Inserir.\n2 - Remover.\n3 - Buscar.\n4 - Imprimir.\n5 - Sair\nSua resposta: ");
-        scanf_s("%d", &resp);
+        printf("\nMENU.\n1 - Inserir.\n2 - Remover.\n3 - Buscar.\n4 - Imprimir.\n5 - Sair\nSua resposta: ");
+        scanf("%d", &resp);
     }
+    /* Liberando memória. */
     esvaziaArvore(arv);
     free(arv);
     arv = NULL;
