d8d5cb2bff3470f503249350d9b9322e179d05e8
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-11-04 13:21:51 -0300

    Update Arvore.h

--- Arvore Binaria/Arvore/Arvore.h
+++ Arvore Binaria/Arvore/Arvore.h
@@ -24,14 +24,14 @@ Descrição: função responsável por retornar a raiz da árvore informada.
 Entrada: ponteiro para a árvore.
 Saída: ponteiro para a raiz.
 */
-no *getRaiz(arvore *arv);
+no *getRaiz(const arvore *arv);
 
 /*
 Descrição: função responsável por retornar a quantidade de elementos na árvore.
 Entrada: ponteiro para a árvore.
 Saída: inteiro da quantidade de elementos na árvore.
 */
-int getNumElementos(arvore *arv);
+int getNumElementos(const arvore *arv);
 
 /*
 Descrição: função responsável por criar e inicializar um nó.
@@ -50,7 +50,7 @@ int insere(arvore *arv, int chave);
 /*
 Descrição: função responsável por remover um elemento da árvore.
 Entrada: ponteiro para a árvore, inteiro da chave.
-Saída: 1 - sucesso, 0 - árvore vazia, -1 - elemento não encontrado.
+Saída: 1 - sucesso, 0 - árvore vazia, −1 - elemento não encontrado.
 */
 int deleta(arvore *arv, int chave);
 
@@ -67,25 +67,25 @@ Entrada: ponteiro para a árvore, inteiro da chave.
 Saída: 1 - encontrou, 0 - não encontrou.
 Observação: o correto seria retornar o nó ou a chave, mas para fins didáticos, a função apenas informa se encontrou ou não.
 */
-int busca(arvore *arv, int chave);
+int busca(const arvore *arv, int chave);
 
 /*
-Descrição: função responsável por imprimir a árvore em pré ordem (raiz, filho esquerdo, filho direito). Se o nó tiver pai, ele será impresso na frente.
+Descrição: função responsável por imprimir a árvore em pré-ordem (raiz, filho esquerdo, filho direito). Se o nó tiver pai, ele será impresso na frente.
 Entrada: ponteiro para o nó (inicialmente a raiz).
 Saída: nada.
 */
-void preOrdem(no* raiz);
+void preOrdem(const no* raiz);
 
 /*
 Descrição: função responsável por imprimir a árvore em ordem (filho esquerdo, raiz, filho direito).
 Entrada: ponteiro para o nó (inicialmente a raiz).
 Saída: nada.
 */
-void emOrdem(no *raiz);
+void emOrdem(const no *raiz);
 
 /*
-Descrição: função responsável por imprimir a árvore em pós ordem (filho esquerdo, filho direito, raiz). Se o nó tiver pai, ele será impresso na frente.
+Descrição: função responsável por imprimir a árvore em pós-ordem (filho esquerdo, filho direito, raiz). Se o nó tiver pai, ele será impresso na frente.
 Entrada: ponteiro para o nó (inicialmente a raiz).
 Saída: nada.
 */
-void posOrdem(no *raiz);
+void posOrdem(const no *raiz);
